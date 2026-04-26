81ccc3b2a1594121870d87cccbb56b1b4845d625
Sharanya <sharanya@gmail.com>
2026-04-26 10:27:55 +0530

    Day 5: Login, AuthContext, ProtectedRoute and pagination

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
-import FormPage from "./pages/FormPage";
+import { AuthProvider } from "./context/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import ListPage from "./pages/ListPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return <FormPage />;
+return ( <AuthProvider> <ProtectedRoute> <ListPage /> </ProtectedRoute> </AuthProvider>
+);
 }
 
 export default App;
